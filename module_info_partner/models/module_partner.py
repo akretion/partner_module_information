006ef484901f6a3b939ba7ab1be67df989868cc6
Florian da Costa <florian.dacosta@akretion.com>
2020-05-01 19:13:06 +0200

    Improve module partner search view

--- module_info_partner/models/module_partner.py
+++ module_info_partner/models/module_partner.py
@@ -6,11 +6,11 @@ from odoo import models, fields, api
 class ModulePartner(models.Model):
     _name = "module.partner"
 
-    partner_id = fields.Many2one("res.partner", required=True, index=True)
+    partner_id = fields.Many2one("res.partner", required=True, index=True, string="Partner")
     version_id = fields.Many2one(
-        "odoo.version", string="Version", required=True, index=True
+        "odoo.version", string="Version", required=True, index=True, string="Version"
     )
-    module_id = fields.Many2one("module.information", required=True, index=True)
+    module_id = fields.Many2one("module.information", required=True, index=True, string="Module")
 
     @api.model
     def _prepare_module_info_vals(self, module_info):
