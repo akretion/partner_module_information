eebb2c49a0fda41da7c11dedd6663819227e31fe
Florian da Costa <florian.dacosta@akretion.com>
2020-05-01 18:37:23 +0200

    Fix available versions + add wip versions

--- module_info_partner/models/module_information.py
+++ module_info_partner/models/module_information.py
@@ -20,23 +20,35 @@ class ModuleInformation(models.Model):
     )
     available_version_ids = fields.Many2many(
         "odoo.version",
+        "available_module_version_rel",
+        "module_id",
+        "version_id",
         string="Available Odoo Versions",
-        compute="_compute_available_version_ids",
+        compute="_compute_version_ids",
+        store=True,
+    )
+    wip_version_ids = fields.Many2many(
+        "odoo.version",
+        "wip_module_version_rel",
+        "module_id",
+        "version_id",
+        string="Work in Progress Versions",
+        compute="_compute_version_ids",
         store=True,
     )
     equivalent_module_ids = fields.Many2many(
         "module.information",
         "equivalent_module_info_rel",
-        "module1",
-        "module2",
+        "module1_id",
+        "module2_id",
         help="Fill the field with modules that replace or are replaced by "
         "this one. (splitted module or renamed...)",
     )
     alternative_module_ids = fields.Many2many(
         "module.information",
         "alternative_module_info_rel",
-        "module1",
-        "module2",
+        "module1_id",
+        "module2_id",
         help="Fill the field with modules that have very similar features with"
         "this one and could replace it.",
     )
@@ -44,7 +56,7 @@ class ModuleInformation(models.Model):
     @api.depends(
         "module_version_ids.state", "equivalent_module_ids.module_version_ids.state"
     )
-    def _compute_available_version_ids(self):
+    def _compute_version_ids(self):
         # TODO for now, it does not take alternative_module_ids into account
         # not sure if it should.
         # more importantly, it seems very hard to take all case into account.
@@ -55,34 +67,35 @@ class ModuleInformation(models.Model):
         # for now, we manage most case and don't care too much about
         # the very special cases.
         # we check if every module version of equivalent modules are done.
+        odoo_versions = self.env['odoo.version'].search([])
         for module in self:
-            version_ids = []
+            available_version_ids = []
+            wip_version_ids = []
             for module_version in module.module_version_ids:
                 if module_version.state == "done":
-                    version_ids.append(module_version.version_id.id)
+                    available_version_ids.append(module_version.version_id.id)
+                elif module_version.state == 'cancel':
+                    wip_version_ids.append(module_version.version_id.id)
+            missing_versions = odoo_versions.filtered(lambda v: v.id not in available_version_ids and v.id not in wip_version_ids)
+            for missing_version in missing_versions:
+                equivalent_modules = module._get_equivalent_modules_from_version(missing_version)
+                if not equivalent_modules:
+                    continue
+                equivalent_module_versions = self.env["module.version"].search(
+                    [
+                        ("module_id", "in", equivalent_modules.ids),
+                        ("version_id", "=", missing_version.id),
+                    ]
+                )
+                if len(equivalent_module_versions) == len(equivalent_modules):
+                    if all([mv.state == 'done' for mv in equivalent_module_versions]):
+                        available_version_ids.append(missing_version.id)
+                    else:
+                        wip_version_ids.append(missing_version.id)
 
-            # if there are equivalent modules, check if they are available
-            # but only for our module's not available versions.
-            equivalent_module_versions = self.env["module.version"].search(
-                [
-                    ("module_id", "in", module.equivalent_module_ids.ids),
-                    ("version_id", "not in", version_ids),
-                ]
-            )
-            pending_versions = equivalent_module_versions.filtered(
-                lambda v: v.state == "pending"
-            )
-            done_versions = equivalent_module_versions.filtered(
-                lambda v: v.state == "done"
-            )
-            # if for one version we have done and pending, we consider the
-            # module has been split and both should be done to be available
-            available = list(
-                set(done_versions.mapped("version_id").ids)
-                - set(pending_versions.mapped("version_id").ids)
-            )
-            version_ids = available + version_ids
-            for eq_module_version in equivalent_module_versions:
-                pass
+            module.available_version_ids = available_version_ids
+            module.wip_version_ids = wip_version_ids
 
-            module.available_version_ids = version_ids
+    def _get_equivalent_modules_from_version(self, version):
+        self.ensure_one()
+        return self.equivalent_module_ids
