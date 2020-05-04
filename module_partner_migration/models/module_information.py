dfb9b7f08bce316dea7a074f802c9746ce3e9fda
Florian da Costa <florian.dacosta@akretion.com>
2020-05-04 23:24:54 +0200

    Add obsolete notion on modules

---
+++ module_partner_migration/models/module_information.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+from odoo import models, fields
+
+
+class ModuleInformation(models.Model):
+    _inherit = "module.information"
+
+    obsolete_version_id = fields.Many2one(
+        'odoo.version',
+        string="Obsolete Version",
+    help=("The version indicated here and all version after that will be "
+          "considered obsolete. The migration status will take it into account"))
