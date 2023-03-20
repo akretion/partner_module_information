6c17c59e103344035c08025dda00d411450eca28
Matthieu <matthieu.saison@akretion.br.com>
2023-03-20 17:26:13 +0100

    Add moduletimesheet
    Add the timesheet to the pr view
    Auto compute the timesheet description

--- module_info_pull_request/models/pull_request.py
+++ module_info_pull_request/models/pull_request.py
@@ -5,28 +5,12 @@ from datetime import datetime
 import requests
 
 from odoo import api, fields, models
-from odoo.osv import expression
 
 _logger = logging.getLogger(__name__)
 
 
 class PullRequest(models.Model):
     _name = "pull.request"
-
-    def _domain_project_id(self):
-        domain = [("allow_timesheets", "=", True)]
-        if not self.user_has_groups("hr_timesheet.group_timesheet_manager"):
-            return expression.AND(
-                [
-                    domain,
-                    [
-                        "|",
-                        ("privacy_visibility", "!=", "followers"),
-                        ("allowed_internal_user_ids", "in", self.env.user.ids),
-                    ],
-                ]
-            )
-        return domain
 
     title = fields.Char(index=True, readonly=True)
     repo_id = fields.Many2one(
@@ -46,27 +30,7 @@ class PullRequest(models.Model):
     number = fields.Integer(index=True, string="Github number", readonly=True)
     author = fields.Char(index=True, readonly=True)
     orga = fields.Char(index=True, readonly=True)
-    timesheet_ids = fields.One2many("account.analytic.line", "task_id", "Timesheets")
-    project_id = fields.Many2one(
-        "project.project",
-        "Project",
-        compute="_compute_project_id",
-        store=True,
-        readonly=False,
-        # domain=_domain_project_id,
-    )
-    task_id = fields.Many2one(
-        "project.task",
-        "Task",
-        compute="_compute_task_id",
-        store=True,
-        readonly=False,
-        index=True,
-        domain="["
-        "('company_id', '=', company_id), "
-        "('project_id.allow_timesheets', '=', True), "
-        "('project_id', '=?', project_id)]",
-    )
+    need_review = fields.Boolean(string="Review requested")
 
     _sql_constraints = [
         (
@@ -75,16 +39,6 @@ class PullRequest(models.Model):
             "the pair pr number and repo must be unique",
         ),
     ]
-
-    @api.depends("task_id", "task_id.project_id")
-    def _compute_project_id(self):
-        for line in self.filtered(lambda line: not line.project_id):
-            line.project_id = line.task_id.project_id
-
-    @api.depends("project_id")
-    def _compute_task_id(self):
-        for line in self.filtered(lambda line: not line.project_id):
-            line.task_id = False
 
     @api.depends("reviewer_ids")
     def _compute_reviewer(self):
---
+++ module_info_timesheet/models/account_analytic_line.py
@@ -0,0 +1,31 @@
+from odoo import api, fields, models
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = "account.analytic.line"
+
+    pr_id = fields.Many2one(
+        comodel_name="pull.request",
+        string="PR",
+        compute="_compute_pr_id",
+        store=True,
+        readonly=False,
+    )
+
+    @api.depends("pr_id")
+    def _compute_pr_id(self):
+        super()._compute_task_id()
+        for record in self:
+            if record.pr_id:
+                record.task_id = record.pr_id.task_id.id
+
+    # Use compute readonly field do not work
+    # TODO retry in v16
+    @api.onchange("name")
+    def _onchange_name(self):
+        for record in self:
+            if record.pr_id:
+                record.name = (
+                    "Review PR"
+                    f"{','.join(record.pr_id.module_ids.mapped('technical_name'))}"
+                )
