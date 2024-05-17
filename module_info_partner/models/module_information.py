d6304e062f9ab9f2adf2c9d0829d6dfb54595ef6
David Beal <david.beal@akretion.com>
2024-05-17 17:34:35 +0200

    IMP module_info_partner: set repo_id writable

--- module_info_partner/models/module_information.py
+++ module_info_partner/models/module_information.py
@@ -15,7 +15,7 @@ class ModuleInformation(models.Model):
     description = fields.Html(readonly=True)
     authors = fields.Char(readonly=True, index=True)
     repo_id = fields.Many2one(
-        "module.repo", readonly=True, index=True, string="Host Repository"
+        "module.repo", index=True, string="Host Repository"
     )
     partner_id = fields.Many2one(
         "res.partner", index=True, string="Partner's Custom Module", readonly=True
